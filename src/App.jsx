15da3bb2d39aaa416e9c0e5acd7a8038caa81b4a
Enrique SFranco <enrique.sfranco04@gmail.com>
2023-04-08 16:48:58 -0600

    Open close principle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import SingleResposibilityPrinciple from './SingleResponsibilityPrinciple'
 import './App.css'
+import OpenClosedPrinciple from './OpenClosedPrinciple'
 
 function App () {
   return (
     <div className='App'>
       <SingleResposibilityPrinciple />
+      <OpenClosedPrinciple />
     </div>
   )
 }
